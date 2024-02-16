0910f887e19c1525ace1f015d437a206ba35cfee
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-02-16 17:09:06 +0300

    16 feb 2024
    update 06

--- gulpfile.js/config/app.js
+++ gulpfile.js/config/app.js
@@ -1,3 +1,5 @@
+const recompress = require('imagemin-jpeg-recompress');
+const pngquant = require('imagemin-pngquant');
 const pugbem = require('gulp-pugbem');
 const isProd = process.argv.includes('--production');
 const isDev = !isProd;
@@ -10,6 +12,7 @@ module.exports = {
 		// mode: isProd ? 'production' : 'development',
 		mode: 'production',
 		entry: {
+			// script: './#src/js/script.js',
 			main: './#src/js/main.js'
 		},
 		output: {
@@ -93,10 +96,31 @@ module.exports = {
 		cascade: false,
 		grid: 'auto-place',
 		overrideBrowserslist: [
-			'last 3 versions'
+			'last 3 versions',
+			'Android >= 5',
+			'Firefox >= 24',
+			'Safari >= 6',
+			'Opera >= 12',
 		],
 	},
 	imagemin: {
-		verbose: true
-	}
+		verbose: true,
+		// interlaced: true,
+		// progressive: true,
+		// optimizationLevel: 5,
+	}[
+		recompress({
+			loops: 6,
+			min: 50,
+			max: 90,
+			quality: 'high',
+			use: [
+				pngquant({
+					quality: [0.8, 1],
+					strip: true,
+					speed: 1,
+				}),
+			],
+		})
+	],
 };
